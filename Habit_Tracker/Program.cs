a6fe44a5088ba48f87249a14c6086bc06fee2261
Jacob Melso <jmelso@wgu.edu>
2025-01-20 21:37:10 -0600

    Add app.db as a data connection

--- Habit_Tracker/Program.cs
+++ Habit_Tracker/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Data.SQLite;
+
+namespace Habit_Tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void CreateDB()
+        {
+            string dbPath = "C:/Users/melso/source/repos/Habit_Tracker/Habit_Tracker/Database/app.db";
+            string connectionString = $"Data Source={dbPath};Version=3";
+            //string connectionString = $"Data Source={dbPath}";
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText = @"
+                    CREATE TABLE IF NOT EXISTS drink_water (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Date TEXT,
+                        Quantity INTEGER
+                        )";
+
+                tableCmd.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+
+    }
+}
