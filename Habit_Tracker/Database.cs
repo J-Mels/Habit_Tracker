c9eb0772696a88165590ce626b294d2c25224c53
Jacob Melso <jmelso@wgu.edu>
2025-01-21 12:12:07 -0600

    Create parameters for CreateTable method in Database class

--- Habit_Tracker/Database.cs
+++ Habit_Tracker/Database.cs
@@ -9,21 +9,23 @@ namespace Habit_Tracker
 {
     internal class Database
     {
-        public static void CreateDB()
-        {
-            string dbPath = "C:/Users/melso/source/repos/Habit_Tracker/Habit_Tracker/Database/app.db";
-            string connectionString = $"Data Source={dbPath};Version=3";
+        private static string connectionString = "Data Source=C:/Users/melso/source/repos/Habit_Tracker/Habit_Tracker/Database/app.db;Version=3";
+        //public static void CreateDB()
+        //{
+        //}
 
+        public static void CreateTable(string Name, string Date, string Quantity)
+        {
             using (var connection = new SQLiteConnection(connectionString))
             {
                 connection.Open();
                 var tableCmd = connection.CreateCommand();
 
-                tableCmd.CommandText = @"
-                            CREATE TABLE IF NOT EXISTS drink_water (
+                tableCmd.CommandText = @$"
+                            CREATE TABLE IF NOT EXISTS {Name} (
                                 Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                Date TEXT,
-                                Quantity INTEGER
+                                {Date} TEXT,
+                                {Quantity} INTEGER
                                 )";
 
                 tableCmd.ExecuteNonQuery();
@@ -32,3 +34,4 @@ namespace Habit_Tracker
         }
     }
 }
+
