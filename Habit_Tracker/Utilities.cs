ca7d1337531d71050572290c8eb213e334fd1f78
Jacob Melso <jmelso@wgu.edu>
2025-01-23 22:05:55 -0600

    Add prompt for user to enter a new habit in CreateHabit

--- Habit_Tracker/Utilities.cs
+++ Habit_Tracker/Utilities.cs
@@ -65,50 +65,36 @@ namespace Habit_Tracker
             while (!returnToMainMenu)
             {
 
-                habitName = UserInput.GetNameInput("Input habit name (No spaces or special characters. Must be no more than 25 characters). Or, enter 0 to return to main menu:");
+                habitName = UserInput.GetNameInput("Input habit name (No spaces or special characters. Must be no more than 25 characters).\nOr, enter 0 to return to main menu:");
                 if (habitName == "0") returnToMainMenu = true;
-                string habitDate = UserInput.GetDateInput("Input habit date (Use mm-dd-yyyy format). Or, enter 0 to return to main menu:");
+                string habitDate = UserInput.GetDateInput("Input habit date (Use mm-dd-yyyy format).\nOr, enter 0 to return to main menu:");
                 if (habitDate == "0") returnToMainMenu = true;
-                string habitQuantity = UserInput.GetQuantityInput("Input habit quantity (Only whole numbers accepted). Or, enter 0 to return to main menu:");
+                string habitQuantity = UserInput.GetQuantityInput("Input habit quantity (Only whole numbers accepted).\nOr, enter 0 to return to main menu:");
                 if (habitQuantity == "0") returnToMainMenu = true;
                 Database.CreateTable(habitName, habitDate, habitQuantity);
 
+                string addNextHabit = UserInput.GetUserInput($"\nHabit Created: {habitName}.\n\nWould you like to create another habit? (Y/N).");
+                while (addNextHabit != "Y" && addNextHabit != "y" && addNextHabit != "N" && addNextHabit != "n")
+                {
+                    if (addNextHabit == "Y" || addNextHabit == "y")
+                    {
+                        continue;
+                    }
+                    else if (addNextHabit == "N" || addNextHabit == "n")
+                    {
+                        returnToMainMenu = true;
+                        break;
+                    }
+                    else
+                    {
+                        addNextHabit = UserInput.GetUserInput("Invalid selection. Enter Y to create another habit or N to return to main menu.");
+                    }
+
+                }
 
             }
 
-            Console.WriteLine($"\nHabit Created: {habitName}. Press any key to continue.");
-            Console.ReadKey();
             DisplayMainMenu();
         }
-        //public static void DisplayUpdateMenu()
-        //{
-        //    string? userInput;
-        //    do
-        //    {
-        //        Console.Clear();
-        //        Console.WriteLine("\n\n-------- UPDATE MENU -------");
-        //        Console.WriteLine("\nPlease select an option:");
-        //        Console.WriteLine("1) Update Habit");
-        //        Console.WriteLine("0) Back to Main Menu");
-
-        //        userInput = Console.ReadLine();
-
-        //        switch (userInput)
-        //        {
-        //            case "1":
-        //                // TODO: Create update functionality
-        //                break;
-        //            case "0":
-        //                break;
-        //            default:
-        //                Console.WriteLine("Invalid selection. Please try again or enter 0 to return to update menu.");
-        //                break;
-        //        }
-
-        //    } while (userInput != "0");
-        //    DisplayMainMenu();
-        //}
-        //public static void DisplayViewMenu() { }
-        //public static void DisplayDeletionMenu() { }
     }
 }
